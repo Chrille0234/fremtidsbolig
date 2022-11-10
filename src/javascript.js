b64c7a7baa3c04905dfd25179897c311d84f6a20
Chrille0234 <madsen.chr@hotmail.com>
2022-11-10 19:48:00 +0100

    changed just about everything:)

--- src/javascript.js
+++ src/javascript.js
@@ -21,3 +21,56 @@ function skiftFugtighed() {
     x = (x + 10)*2; 
     document.getElementById("fugtighed").innerHTML = x + "%";
 }
+
+function powerOff() {
+    var color = document.getElementById("power")
+    color.classList.toggle("internetstrømvarmegrøn");
+    color.classList.toggle("internetstrømvarmerød");
+
+    var text = document.getElementById("powerText")
+    if (text.innerHTML === "tændt"){
+        text.innerHTML = "slukket"
+    }
+    else{
+        text.innerHTML = "tændt"
+    }
+
+  }
+  function netOff() {
+    var color = document.getElementById("net")
+    color.classList.toggle("internetstrømvarmegrøn");
+    color.classList.toggle("internetstrømvarmerød");
+
+    var text = document.getElementById("netText")
+    if (text.innerHTML === "tændt"){
+        text.innerHTML = "slukket"
+    }
+    else{
+        text.innerHTML = "tændt"
+    }
+
+  }
+  function warmOff() {
+    var color = document.getElementById("warm")
+    color.classList.toggle("internetstrømvarmegrøn");
+    color.classList.toggle("internetstrømvarmerød");
+
+    var text = document.getElementById("warmText")
+    if (text.innerHTML === "tændt"){
+        text.innerHTML = "slukket"
+    }
+    else{
+        text.innerHTML = "tændt"
+    }
+
+  }
+  function doorLock() {
+    var color = document.getElementById("door")
+    color.classList.toggle("internetstrømvarmegrøn");
+    color.classList.toggle("internetstrømvarmerød");
+
+    var icon = document.getElementById("doorLock")
+    icon.classList.toggle("fa-lock");
+    icon.classList.toggle("fa-lock-open")
+
+  }
