2fc27ae7efd05d63d4a622752d06cf481896acca
chrille0234 <madsen.chr@hotmail.com>
2022-11-10 11:29:42 +0100

    added new shit to the info page

--- src/javascript.js
+++ src/javascript.js
@@ -1,4 +1,9 @@
-setInterval(skiftTemp, 5000);
+setInterval(runAll, 5000);
+function runAll(){
+    skiftFugtighed();
+    skiftTemp();
+}
+
 
 function skiftTemp() {
     let x = Math.floor(Math.random() * 20) + 10;
@@ -7,4 +12,12 @@ function skiftTemp() {
     if (x > 17){document.getElementById("temp").innerHTML = x + "°";}
     // sørger for at temperaturen er over 17 grader
     else{ x = x + 10; document.getElementById("temp").innerHTML = x + "°";}
+}
+
+function skiftFugtighed() {
+    let x = Math.floor(Math.random() * 20) + 10;
+
+    
+    x = (x + 10)*2; 
+    document.getElementById("fugtighed").innerHTML = x + "%";
 }
